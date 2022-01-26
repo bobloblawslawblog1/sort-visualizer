5ec57df0f19ab08fb429bf76a148a3fe765d8f2a
bobloblawslawblog1 <parkerveit@gmail.com>
2022-01-26 13:37:11 -0500

    combine unecessary components into main component

--- src/components/Visualizer.js
+++ src/components/Visualizer.js
@@ -1,34 +1,61 @@
-import Nav from './Nav';
-import ListElement from './ListElement';
+
 import './Visualizer.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import mergeSort from '../scripts/mergeSort';
 import React from 'react';
 
 
 
-function Visualizer(props) {
+function Visualizer() {
   const [arrSize, setArrSize] = useState(10);
-  const [arr,setArr] = useState(randomArr(arrSize));
-  
-  
-  function handleMergeBtn(){
+  const [arr,setArr] = useState([]);
+
+  const handleMergeBtn = () => {
+    console.log(arr);
     mergeSort(arr,0,arr.length-1);
     setArr(arr);
     console.log(arr);
+  }
+
+  function randomArr(len){
+    let arr = [];
+    for(let i = 0; i < len; i++){
+      arr[i] = getRandomInt();
+    }
+    setArr(arr);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setArrSize(arrSize);
+    randomArr(arrSize);
   }
   
   return (
     <div className="App">
       <div className='View'> 
-        {Object.keys(arr).map((keys) => <ListElement key={keys} value={arr[keys]}/>)}
+        {arr.map((element,index) => <div key={index} style={{height: `${element/10}%`, background: "#77448f",
+         alignSelf: "flex-end",flex: "1 0 0px", overflow: "hidden", margin: "0 .5px"}}>{element}</div>)}
       </div>
-      <Nav value={arrSize} stateChange={setArrSize} mergeBtn={handleMergeBtn}/>
+      <div className='navbar'>
+            <button onClick={handleMergeBtn}>Merge Sort</button>
+            <button>Quick Sort</button>
+            <button>Bubble Sort</button>
+            <button>Insertion Sort</button>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Array Size:
+                    <input type="number" className='value' min="10" max="500" name='arrSize' value={arrSize} onChange={(e) => setArrSize(e.target.value)}/>
+                    <input type="range" min="10" max="500" step="1" name='arrSize' value={arrSize} onChange={(e) => setArrSize(e.target.value)}/>
+                </label> 
+                <input type="submit" value="Enter"/>
+            </form>
+            
+        </div>
     </div>
   );
 }
 export default Visualizer;
-
 
 
 
@@ -39,10 +66,4 @@ function getRandomInt(){
   return Math.floor(Math.random() * (max - min) + min); 
 }
 
-function randomArr(len){
-  let arr = [];
-  for(let i = 0; i < len; i++){
-    arr[i] = getRandomInt();
-  }
-  return arr;
-}
+
