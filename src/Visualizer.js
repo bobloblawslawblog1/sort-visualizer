1e3ae480e4ea4e1a73db701b3be1570fb445e9c9
bobloblawslawblog1 <parkerveit@gmail.com>
2022-01-27 16:39:53 -0500

    cleaned up files.

--- src/Visualizer.js
+++ src/Visualizer.js
@@ -1,7 +1,7 @@
-
 import './Visualizer.css';
 import { useState } from 'react';
-import mergeSort from '../scripts/mergeSort';
+import mergeSort from './scripts/mergeSort';
+import quickSort from './scripts/quickSort';
 import React from 'react';
 
 
@@ -9,12 +9,19 @@ import React from 'react';
 function Visualizer() {
   const [arrSize, setArrSize] = useState(10);
   const [arr,setArr] = useState([]);
-
-  const handleMergeBtn = () => {
+  
+  const handleMsBtn = () => {
     console.log(arr);
     mergeSort(arr,0,arr.length-1);
     setArr(arr);
     console.log(arr);
+  }
+
+  const handleQsBtn = () => {
+    console.log(arr);
+    quickSort(arr,0,arr.length-1);
+    setArr(arr);
+    document.getElementsByClassName('element')[0].style.background = "red";
   }
 
   function randomArr(len){
@@ -34,12 +41,12 @@ function Visualizer() {
   return (
     <div className="App">
       <div className='View'> 
-        {arr.map((element,index) => <div key={index} style={{height: `${element/10}%`, background: "#77448f",
-         alignSelf: "flex-end",flex: "1 0 0px", overflow: "hidden", margin: "0 .5px"}}>{element}</div>)}
+        {arr.map((element,index) => <div key={index} className='element' style={{height: `${element/10}%`, background: "#77448f",
+         alignSelf: "flex-end",flex: "1 0 0px", overflow: "hidden", margin: "0 .5px"}}></div>)}
       </div>
       <div className='navbar'>
-            <button onClick={handleMergeBtn}>Merge Sort</button>
-            <button>Quick Sort</button>
+            <button onClick={handleMsBtn}>Merge Sort</button>
+            <button onClick={handleQsBtn}>Quick Sort</button>
             <button>Bubble Sort</button>
             <button>Insertion Sort</button>
             <form onSubmit={handleSubmit}>
@@ -60,7 +67,7 @@ export default Visualizer;
 
 
 //from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(){
+export function getRandomInt(){
   let min = Math.ceil(5);
   let max = Math.floor(1000);
   return Math.floor(Math.random() * (max - min) + min); 
